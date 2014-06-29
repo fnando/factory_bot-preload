55c14f39d00f70978a4ae3a3939ecc6d4b897ef6
Nando Vieira <fnando.vieira@gmail.com>
2014-06-29 11:53:48 -0300

    Bump up version.

--- lib/factory_girl/preload/version.rb
+++ lib/factory_girl/preload/version.rb
@@ -2,8 +2,8 @@ module FactoryGirl
   module Preload
     module Version
       MAJOR = 2
-      MINOR = 1
-      PATCH = 1
+      MINOR = 2
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
   end
