468d4e4f237b2640a691af663b48e81be36b57a1
Nando Vieira <me@fnando.com>
2019-11-26 18:06:55 -0800

    Bump up version.

--- lib/factory_bot/preload/version.rb
+++ lib/factory_bot/preload/version.rb
@@ -4,8 +4,8 @@ module FactoryBot
   module Preload
     module Version
       MAJOR = 0
-      MINOR = 1
-      PATCH = 1
+      MINOR = 2
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
   end
