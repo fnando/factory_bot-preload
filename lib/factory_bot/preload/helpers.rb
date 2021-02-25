63f8de479a884ce619ef22a765bb1669e298f85d
ermolaev <andruhafirst@yandex.ru>
2021-02-26 00:05:06 +0300

    add fixture_stub_const, refactor fixture set

--- lib/factory_bot/preload/helpers.rb
+++ lib/factory_bot/preload/helpers.rb
@@ -27,38 +27,37 @@ module FactoryBot
         ::FactoryBot::Preload::Helpers.define_helper_methods
       end
 
-      def fixture(name, model = nil, &block)
-        if block_given?
-          fixture_set(name, &block)
-        else
-          fixture_get(name, model)
-        end
-      end
-      alias :factory :fixture
-
-      private def fixture_get(name, model)
-        fixture = Preload.factories[model.name][name]
-
-        if fixture.blank? && Preload.factories[model.name].key?(name)
-          fixture = model.find(Preload.record_ids[model.name][name])
-          Preload.factories[model.name][name] = fixture
-        end
-
-        unless fixture
-          raise "Couldn't find #{name.inspect} factory for #{model.name.inspect} model"
-        end
-
-        fixture
-      end
-
-      private def fixture_set(name, &block)
-        record = instance_eval(&block).freeze
-        Preload.factories[record.class.name] ||= {}
-        Preload.factories[record.class.name][name.to_sym] = record
+      def fixture(name, &block)
+        record = instance_eval(&block)
 
         Preload.record_ids[record.class.name] ||= {}
         Preload.record_ids[record.class.name][name.to_sym] = record.id
         record
+      end
+
+      def fixture_stub_const(name, const_name, &block)
+        record = fixture(name, &block)
+        model = record.class
+        if model.const_defined?(const_name)
+          model.send(:remove_const, const_name)
+          model.const_set(const_name, record.id)
+        else
+          raise ArgumentError, "#{model.name}::#{const_name} is not defined"
+        end
+        record
+      end
+
+      private def fixture_get(name, model)
+        per_test_key = "#{model.name}-#{name}"
+        fixture = Preload.fixtures_per_test[per_test_key]
+
+        if fixture
+          fixture
+        elsif (record_id = Preload.record_ids[model.name][name])
+          Preload.fixtures_per_test[per_test_key] = model.find(record_id)
+        else
+          raise "Couldn't find #{name.inspect} fixture for #{model.name.inspect} model"
+        end
       end
     end
   end
